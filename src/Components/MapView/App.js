edc2e8d9455295bc881a4be99bd6f1e2bf850000
miyass <exe.miyakura0629@gmail.com>
2018-02-03 15:37:37 -0800

    Japan Map is done. Next Task is USA map

--- src/Components/MapView/App.js
+++ src/Components/MapView/App.js
@@ -4,6 +4,8 @@ import * as topojson from "topojson-client";
 
 import JapanJson from '../../Json/MapView/japan.json';
 
+import '../../Styles/MapView/App.css';
+
 
 class App extends Component {
 
@@ -12,80 +14,37 @@ class App extends Component {
   }
 
   componentDidMount() {
-    drawMap();
+    drawJapanMap();
   }
 
   render() {
 
     return(
       <div>
+        <div className="mapAll">
+          <div className="japan">
+          </div>
+        </div>
+        <h1>旅行Map</h1>
       </div>
     );
   }
 }
 
-function drawMap() {
-  var pref = {
-    "1":"北海道",
-    "2":"青森県",
-    "3":"岩手県",
-    "4":"宮城県",
-    "5":"秋田県",
-    "6":"山形県",
-    "7":"福島県",
-    "8":"茨城県",
-    "9":"栃木県",
-    "10":"群馬県",
-    "11":"埼玉県",
-    "12":"千葉県",
-    "13":"東京都",
-    "14":"神奈川県",
-    "15":"新潟県",
-    "16":"富山県",
-    "17":"石川県",
-    "18":"福井県",
-    "19":"山梨県",
-    "20":"長野県",
-    "21":"岐阜県",
-    "22":"静岡県",
-    "23":"愛知県",
-    "24":"三重県",
-    "25":"滋賀県",
-    "26":"京都府",
-    "27":"大阪府",
-    "28":"兵庫県",
-    "29":"奈良県",
-    "30":"和歌山県",
-    "31":"鳥取県",
-    "32":"島根県",
-    "33":"岡山県",
-    "34":"広島県",
-    "35":"山口県",
-    "36":"徳島県",
-    "37":"香川県",
-    "38":"愛媛県",
-    "39":"高知県",
-    "40":"福岡県",
-    "41":"佐賀県",
-    "42":"長崎県",
-    "43":"熊本県",
-    "44":"大分県",
-    "45":"宮崎県",
-    "46":"鹿児島県",
-    "47":"沖縄県"
-  };
-
+function drawJapanMap() {
   var w = 1300;
   var h = 960;
-  var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+  //japanclass下にsvg要素の作成
+  var svg = d3.select(".japan").append("svg").attr("width", w).attr("height", h);
+  //投影方法？
   var projection = d3.geo.mercator().center([136, 35.5]).scale(2000).translate([w / 2, h / 2]);
   var path = d3.geo.path().projection(projection);
   var topo = topojson.feature(JapanJson, JapanJson.objects.pref).features;
-  console.log(topo);
-  svg.selectAll(".pref").data(topo).enter().append("path").attr("d", path);;
-  // d3.json(JapanJson, function(error, japan) {
-  //   console.log(japan);
-  // });
+  // console.log(topo);
+  svg.selectAll(".japan").data(topo).enter().append("path").attr("class", function(d){
+    return  d.properties.name_local;
+  }).attr("d", path);;
 }
 
 export default App;
