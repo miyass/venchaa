2b94f3a82207732f2670f53c4051751efe22c12b
miyass <exe.miyakura0629@gmail.com>
2018-02-03 14:35:38 -0800

    Japan Map Projection is donegit add .

--- src/Routes/Origin.js
+++ src/Routes/Origin.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import PortfolioApp from '../Components/Portfolio/App';
+import MapViewApp from '../Components/MapView/App';
 
 
 class Origin extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={PortfolioApp} />
+        <div>
+          <Route exact path="/" component={PortfolioApp} />
+          <Route exact path="/map" component={MapViewApp} />
+        </div>
       </Router>
     );
   }
